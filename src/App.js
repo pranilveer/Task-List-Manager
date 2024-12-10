54b64ae85b2f8c11e8ca54bb4ba9a94219cb2703
Pranil Dilip Veer <veerpranil@gmail.com>
2024-12-10 16:47:19 +0530

    Task Form Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import react, { useState } from 'react';
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newtask) => {
+    setTasks([...tasks, { ...newtask, id: tasks.length + 1 }]);
+    toast.success("Task Added!");
+  }
   return (
     <div className="App">
-
+      <h1>Task Manager</h1>
+      <AddTaskForm addTask={addTask} />
+      <ToastContainer />
     </div>
   );
 }
