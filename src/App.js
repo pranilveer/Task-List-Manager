cecafa2add0520e56fb1f98c3c3e81fe2e0e3dc9
Pranil Dilip Veer <veerpranil@gmail.com>
2024-12-10 23:28:19 +0530

    Filter Added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("All");
+
 
   const addTask = (newtask) => {
     setTasks([...tasks, { ...newtask, id: tasks.length + 1 }]);
@@ -15,6 +17,18 @@ const App = () => {
     <div className="App">
       <h1>Task Manager</h1>
       <AddTaskForm addTask={addTask} />
+      <div className="filter-container">
+        <label>Filter by Status: </label>
+        <select
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="To Do">To Do</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Done">Done</option>
+        </select>
+      </div>
       <ToastContainer />
     </div>
   );
